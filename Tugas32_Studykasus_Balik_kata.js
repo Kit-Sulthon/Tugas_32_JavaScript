8ea7f29c0e5ccfe33699f60ed13722e485b9c4ce
T R A $ H <sulthons.a19@gmail.com>
2023-10-06 09:08:06 +0700

    Tugas 32 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ Tugas32_Studykasus_Balik_kata.js
@@ -0,0 +1,16 @@
+function balikKata(kata) {
+  return kata.split("").reverse().join("");
+}
+function balikKata2(kata2) {
+  return kata2.split("").reverse().join("");
+}
+function balikKata3(kata3) {
+  return kata3.split("").reverse().join("");
+}
+function balikKata4(kata4) {
+  return kata4.split("").reverse().join("");
+}
+console.log(balikKata("Niomic!"));
+console.log(balikKata2("JavaScript"));
+console.log(balikKata3("alohahola"));
+console.log(balikKata4("Jawa_Barat"));
